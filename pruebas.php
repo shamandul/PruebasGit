3da81d17c982cdd461ca523250a8e663d883f2f1
Jesús S.P <shamandul@gmail.com>
2023-03-21 21:28:38 +0100

    commit desde nvim

--- pruebas.php
+++ pruebas.php
@@ -1,7 +1,11 @@
 <?php
+
 /**
- * Prueba
+ * @file
+ * Pruebas.
  */
+
+require('include/simple_html_dom.php');
 
 echo "pruebas";
 echo "prueba2";
@@ -10,3 +14,16 @@ echo "prueba5";
 echo "Después del commit";
 echo "Después del commit P7";
 echo "Después del commit P8";
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, 'https://www.google.com/search?q=aplicaciones+php+gratis');
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($curl);
+curl_close($curl);
+//echo $result;
+
+$domResult = new simple_html_dom();
+$domResult->load($result);
+
+foreach($domResult->find('a[href^=/url?]') as $link)
+echo '<h1>' . $link->plaintext . ' </h1><br>';
